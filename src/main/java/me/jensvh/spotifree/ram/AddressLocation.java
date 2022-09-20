3d3f61a1d82085efe1975274205800665bbe3461
jensvh <hummiejens@hotmail.com>
2022-09-20 12:39:33 +0200

    Add bearer location in ram

---
+++ src/main/java/me/jensvh/spotifree/ram/AddressLocation.java
@@ -0,0 +1,89 @@
+package me.jensvh.spotifree.ram;
+
+public class AddressLocation {
+	
+	/**
+	 * Pointer 1:
+	 *   base: spotify.exe + 012FA3E4
+	 *   offset 1: A0
+	 *   offset 2: 180
+	 *   offset 3: 3B0
+	 *   offset 4: 38
+	 *   offset 5: 8
+	 *   offset 6: 100
+	 *   offset 7: 0
+	 *   
+	 * Pointer 2:
+	 *   base: spotify.exe + 012FA3E4
+	 *   offset 1: A0
+	 *   offset 2: 180
+	 *   offset 3: 3B0
+	 *   offset 4: 38
+	 *   offset 5: 4
+	 *   offset 6: F0
+	 *   offset 7: 0
+	 * 
+	 * Pointer 3:
+	 *   base: spotify.exe + 012FA3E4
+	 *   offset 1: A0
+	 *   offset 2: 180
+	 *   offset 3: 3B0
+	 *   offset 4: 3C
+	 *   offset 5: 10
+	 *   offset 6: F0
+	 *   offset 7: 0
+	 * 
+	 * Pointer 4:
+	 *   base: spotify.exe + 012FA3E4
+	 *   offset 1: A0
+	 *   offset 2: 180
+	 *   offset 3: 3B0
+	 *   offset 4: 3C
+	 *   offset 5: 14
+	 *   offset 6: 100
+	 *   offset 7: 0
+	 * 
+	 * Terrible pointers, working, but tend to be null sometimes
+	 * Pointer 5:
+	 *   base: spotify.exe + 0130C9E8
+	 *   offset 1: 60
+	 *   offset 2: 14
+	 *   offset 3: F4
+	 *   offset 4: 44
+	 *   offset 5: 14
+	 *   offset 6: 100
+	 *   offset 7: 0
+	 * 
+	 * Pointer 6:
+	 *   base: spotify.exe + 0130C9E8
+	 *   offset 1: 60
+	 *   offset 2: 14
+	 *   offset 3: F4
+	 *   offset 4: 40
+	 *   offset 5: 4
+	 *   offset 6: F0
+	 *   offset 7: 0
+	 * 
+	 * Pointer 7:
+	 *   base: spotify.exe + 0130C9E8
+	 *   offset 1: 60
+	 *   offset 2: 14
+	 *   offset 3: F4
+	 *   offset 4: 44
+	 *   offset 5: 10
+	 *   offset 6: F0
+	 *   offset 7: 0
+	 * 
+	 * Pointer 7:
+	 *   base: spotify.exe + 0130C9E8
+	 *   offset 1: 60
+	 *   offset 2: 14
+	 *   offset 3: F4
+	 *   offset 4: 40
+	 *   offset 5: 8
+	 *   offset 6: 100
+	 *   offset 7: 0
+	 * 
+	 */
+
+}
